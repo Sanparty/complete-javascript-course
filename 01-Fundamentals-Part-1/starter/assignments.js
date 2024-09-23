0f212021abb556bbdfba535c856664700d319d6b
Sanparty <88841545+Sanparty@users.noreply.github.com>
2024-09-23 17:12:19 -0400

    Module 1 changes

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,36 @@
+
+const country = "Canada";
+const continent = "North America";
+let population = 40;
+
+console.log(country);
+console.log(continent);
+console.log(population + " million")
+
+
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = "English"
+
+const halfPopulation = population / 2
+
+population ++
+console.log(population)
+
+const isGreaterThanFinland = population > 6;
+
+console.log(isGreaterThanFinland)
+
+const isLessThanAverage = population < 33;
+
+console.log(isLessThanAverage);
+
+const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language + '.'
+
+console.log(description)
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,117 @@
+/*
+let js = 'amazing';
+console.log(40 + 0 + 23 - 10 + ' ' + js);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = 'Dan';
+let lastName = 'San Pedro';
+let wholeName = firstName + ' ' + lastName;
+
+function changeName() {
+    let newName = document.getElementById('newName').value
+    if (newName) {
+        document.getElementById('name').innerText = newName;
+        console.log(newName);
+        console.log(newName);
+        console.log(newName);
+    } else {
+        alert('Enter a name please!')
+    }
+    
+}
+
+let javascriptIsFun = true;
+console.log(typeof true);
+
+console.log(typeof javascriptIsFun);
+
+console.log(typeof 32);
+
+console.log(typeof 'Dan')
+
+javascriptIsFun = 'dan';
+
+console.log(typeof javascriptIsFun);
+
+let year;
+
+console.log(year);
+console.log(typeof year);
+
+year = 1991
+
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null)
+
+let age = 30;
+
+age = 31;
+
+const birthYear = 1975
+
+// birthYear = 1990
+
+// const job;
+
+var job = 'editor';
+
+job = 'developer'
+
+lastName = "San Pedro";
+console.log(lastName)
+
+// math operators
+const currentYear = 2040
+
+const ageDan = currentYear - 1975;
+const ageAnn = currentYear - 1976;
+console.log(ageDan, ageAnn);
+
+console.log(ageDan * 2, ageDan / 2, ageDan % 2, 2 ** 3);
+
+const firstName = 'Dan';
+const lastName = 'San Pedro';
+
+console.log(firstName + ' ' + lastName)
+
+// assignment operators
+let x = 10 + 5; // 15
+x += 10 // x = x + 10
+x -= 10 // x = x - 10
+x *= 3 // x * 3
+x /= 5
+x++ // x = x + 1 
+x-- // x = x - 1
+console.log(x)
+
+// comparison operators
+
+console.log(ageDan > ageAnn)
+console.log(ageAnn >= 21)
+
+const isFullAge = ageSarah >= 21;
+*/
+
+const currentYear = 2024
+
+const ageDan = currentYear - 1975;
+const ageAnn = currentYear - 1976;
+
+console.log(currentYear - 1975 > currentYear - 1976)
+
+
+let x, y;
+
+x = y = 25 - 10 - 5 // x = y = 10, x = 10
+
+console.log(x, y)
+
+const averageAge = (ageAnn + ageDan) / 2
+
+console.log('the average age is ' + averageAge)
+
+console.log(ageAnn, ageDan);
