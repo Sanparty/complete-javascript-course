96b5612177459a4a2e146240267a535ec8c944f7
Sanparty <dsanpedro@gmail.com>
2024-10-07 16:42:38 -0400

    while loops

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -309,7 +309,7 @@ console.log(challenge);
 //     console.log(`Lifting weights repetition ${rep}`);
 // };
 
-*/
+
 const danArray = [
     'Dan',
     'Sam Pedro',
@@ -409,3 +409,26 @@ for (let i = 0; i < exercises.length; i++) {
         console.log(`${exercises[i]}: Repetition ${rep}`);
     };
 }
+
+
+
+for (let rep = 1; rep <= 10; rep++) {
+        console.log(`Lifting weights repetition ${rep}`);
+    };
+*/
+
+let rep = 1;
+while (rep <= 10) {
+    // console.log(`WHILE: Lifting weights repetition ${rep}`);
+    rep++;
+}
+
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}.`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log(`Loop is about to end...`)
+} 
