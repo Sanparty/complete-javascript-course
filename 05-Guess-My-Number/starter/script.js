0744096898fb165eed1bcf4b65a1d7198cce7a9f
Sanparty <88841545+Sanparty@users.noreply.github.com>
2024-10-17 22:48:32 -0400

    some changes

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -16,12 +16,47 @@
 
 */
 
+const secretNumber = Math.trunc(Math.random()*20)+1;
+
+let score = 20; // this is a state variable
+
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
     if(!guess) {
+        // When there is no input
         document.querySelector('.message').textContent = '⛔️ No Number entered!';
+
+        // When player wins
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+        // When guess is too high
+    } else if (guess > secretNumber) {
+        if (score > 1 ) {
+            document.querySelector('.message').textContent = '📈 Too high!';
+            score --;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!💥';
+            document.querySelector('.score').textContent = 0;
+        }
+
+        // When guess is too low
+    } else if (guess < secretNumber) {
+        if (score > 1 ) {
+            document.querySelector('.message').textContent = '📉 Too low!';
+            score --;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!💥';
+            document.querySelector('.score').textContent = 0;
+        }
     }
 });
+
