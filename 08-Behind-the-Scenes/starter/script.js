bb9d618e26c59c688a2aa5fa3d9d5a490f49c778
Sanparty <dsanpedro@gmail.com>
2024-10-29 16:45:02 -0400

    started scoping

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2024 - birthYear;
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+    }
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Dan';
+
+calcAge(1975);
