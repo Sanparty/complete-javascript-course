92c2fe3c939f4d240c43c3a435b874b46ed98202
Sanparty <88841545+Sanparty@users.noreply.github.com>
2024-10-30 16:25:13 -0400

    functions scoped

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -3,13 +3,27 @@
 function calcAge(birthYear) {
   const age = 2024 - birthYear;
   function printAge() {
-    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
     console.log(output);
 
     if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
       const str = `Oh, and you're a millenial, ${firstName}`;
       console.log(str);
+    } else {
+      const firstName = 'Steven';
+      var millenial = false;
+      const str = `Oh, you're not a millenial, ${firstName}`;
+      console.log(str);
+      function add(a,b) {
+        return a + b;
+      }
+      output = 'NEW OUTPUT!'
+      
     }
+    console.log(millenial);
+    // console.log(add(2,3)); 
+    console.log(output);
   }
   printAge();
   return age;
