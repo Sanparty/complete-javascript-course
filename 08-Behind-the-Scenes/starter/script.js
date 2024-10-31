9a3d6607cd67a732e614724424ef6bdef1336d0b
Sanparty <dsanpedro@gmail.com>
2024-10-31 16:26:56 -0400

    finished hosting topic

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -15,14 +15,13 @@ function calcAge(birthYear) {
       var millenial = false;
       const str = `Oh, you're not a millenial, ${firstName}`;
       console.log(str);
-      function add(a,b) {
+      function add(a, b) {
         return a + b;
       }
-      output = 'NEW OUTPUT!'
-      
+      output = 'NEW OUTPUT!';
     }
     console.log(millenial);
-    // console.log(add(2,3)); 
+    // console.log(add(2,3));
     console.log(output);
   }
   printAge();
@@ -32,3 +31,36 @@ function calcAge(birthYear) {
 const firstName = 'Dan';
 
 calcAge(1975);
+
+// functions
+
+console.log(addDecl(2, 3));
+// console.log(addExp(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExp = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// example
+let numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products are deleted');
+}
+
+if (!numProducts) deleteShoppingCart();
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
