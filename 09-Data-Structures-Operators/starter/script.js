4b0cf4ba77d827e3cb53fb6e9284b0789fffaf54
Sanparty <dsanpedro@gmail.com>
2025-02-17 14:40:46 -0500

    finished code challenge 4

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -49,6 +49,70 @@ const restaurant = {
   },
 };
 
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  console.log(rows);
+  const results = [];
+  for (const r of rows) {
+    const newRow = r.toLowerCase().split('_');
+    console.log(newRow);
+    const secondWord = newRow[1];
+    const finalWord =
+      newRow[0] + secondWord[0].toUpperCase() + secondWord.slice(1);
+    results.push(finalWord.trim());
+  }
+  let longestString = '';
+  for (let i = 0; i < results.length; i++) {
+    if (
+      typeof results[i] === 'string' &&
+      results[i].length > longestString.length
+    ) {
+      longestString = results[i];
+    }
+  }
+  for (let i = 0; i < results.length; i++) {
+    console.log(
+      results[i].padEnd(longestString.length + 4, ' ') + '✅'.repeat(i + 1)
+    );
+  }
+});
+
+/*
 // Working with Strings Part 3
 
 // Split and join
@@ -108,7 +172,7 @@ planesInLine(5);
 planesInLine(3);
 planesInLine(12);
 
-/*
+
 // Working with Strings Part 2
 
 const airline = 'TAP Air Portugal';
