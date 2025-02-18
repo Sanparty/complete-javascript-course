48223858cf5952474da0b700edb004ae8d4917ba
Sanparty <dansp@me.com>
2025-02-17 22:12:07 -0500

    finished module

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 // Data needed for first part of the section
 
 const weekdays = ['mon', 'tue', 'wed', 'thurs', 'fri', 'sat', 'sun'];
@@ -49,6 +45,27 @@ const restaurant = {
   },
 };
 
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const getCode = flight => flight.slice(0, 3).toUpperCase();
+document.body.append(document.createElement('ul'));
+const list = document.querySelector('ul');
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(44);
+  const listItem = document.createElement('li');
+  listItem.innerText = output;
+  list.append(listItem);
+}
+
 // Coding Challenge #4
 
 /* 
@@ -79,7 +96,7 @@ Afterwards, test with your own test data!
 
 GOOD LUCK 😀
 */
-
+/*
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
